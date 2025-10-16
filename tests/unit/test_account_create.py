1ff1eaac464ce92ff9c8c724e2abf0405d46927b
Efistoffeles <mikrosthaj@gmail.com>
2025-10-16 13:21:51 +0200

    pesel

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,6 +3,9 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe")
+        account = Account("John", "Doe", 0.0, "123456789")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
+        assert account.balance == 0.0
+        assert account.pesel == "123456789"
+        assert len(account.pesel) == 9
