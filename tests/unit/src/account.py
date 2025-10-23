9b6a599d1917ae82cc270d389cf52559334474b4
Efistoffeles <mikrosthaj@gmail.com>
2025-10-23 13:20:12 +0200

    feature7

--- tests/unit/src/account.py
+++ tests/unit/src/account.py
@@ -14,3 +14,20 @@ class Account:
 
     def incoming_transfer(self, amount):
         self.balance += amount
+
+
+class CompanyAccount:
+    def __init__(self, company_name, balance, nip):
+        self.company_name = company_name
+        self.balance = balance
+        if len(str(nip)) != 10:
+            self.nip = "Invalid"
+        else:
+            self.nip = nip
+
+    def outgoing_transfer(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+
+    def incoming_transfer(self, amount):
+        self.balance += amount
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -1,4 +1,4 @@
-from src.account import Account
+from src.account import Account, CompanyAccount
 
 
 class TestAccount:
@@ -45,3 +45,49 @@ class TestAccountTransfer:
         account = Account("Charlie", "White", 0.0, "12345678905")
         account.incoming_transfer(100.0)
         assert account.balance == 100.0
+
+class TestCompanyAccount:
+    def test_company_account_creation(self):
+        account = CompanyAccount("Tech Solutions", 1000.0, "1234567890")
+        assert account.company_name == "Tech Solutions"
+        assert account.balance == 1000.0
+        assert account.nip == "1234567890"
+
+    def test_nip_too_long(self):
+        account = CompanyAccount("Big Corp", 500.0, "12345678901234")
+        assert account.nip == "Invalid"
+
+    def test_nip_too_short(self):
+        account = CompanyAccount("Small Biz", 200.0, "12345")
+        assert account.nip == "Invalid"
+
+    def test_nip_none(self):
+        account = CompanyAccount("Mystery Inc", 300.0, None)
+        assert account.nip == "Invalid"
+
+
+class TestCompanyAccountTransfer:
+    def test_outgoing_transfer_success(self):
+        account = CompanyAccount("Tech Corp", 1000.0, "1234567890")
+        account.outgoing_transfer(300.0)
+        assert account.balance == 700.0
+
+    def test_outgoing_transfer_insufficient_funds(self):
+        account = CompanyAccount("Startup Ltd", 100.0, "9876543210")
+        account.outgoing_transfer(200.0)
+        assert account.balance == 100.0
+
+    def test_outgoing_transfer_exact_balance(self):
+        account = CompanyAccount("Final Corp", 500.0, "1111111111")
+        account.outgoing_transfer(500.0)
+        assert account.balance == 0.0
+
+    def test_incoming_transfer(self):
+        account = CompanyAccount("Rich Inc", 2000.0, "2222222222")
+        account.incoming_transfer(500.0)
+        assert account.balance == 2500.0
+
+    def test_incoming_transfer_zero_balance(self):
+        account = CompanyAccount("New Venture", 0.0, "3333333333")
+        account.incoming_transfer(1000.0)
+        assert account.balance == 1000.0
